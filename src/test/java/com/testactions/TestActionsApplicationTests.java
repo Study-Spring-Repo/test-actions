71813981157605c32b65f370bd7705abf6839453
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-07-15 14:35:22 +0900

    init: test-actions project

---
+++ src/test/java/com/testactions/TestActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
